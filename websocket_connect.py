ff300cd54695f3bbe972d16c1491f0ee2dfee21c
charissalaw <charissa@density.io>
2018-04-24 22:21:01 -0400

    reconnect on websocket disconnect

--- websocket_connect.py
+++ websocket_connect.py
@@ -10,35 +10,46 @@ api_token = os.environ.get('WEBSOCKET_TOKEN', None)
 if api_token is None:
     raise Exception("Please set the 'WEBSOCKET_TOKEN' environment variable")
 
+sound_directory = os.environ.get('SOUND_DIRECTORY', None)
+if api_token is None:
+    raise Exception("Please set the 'SOUND_DIRECTORY' environment variable")
+
 api_headers = {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer {}'.format(api_token),
 }
 
-def connect_to_websocket():
+sound_options = [ "steve_laugh"]
+
+def new_websocket_url():
     api_socket_response = requests.post(
         'https://api.density.io/v2/sockets/',
         headers=api_headers
     ).json()
-    websocket_url = api_socket_response['url']
+    return api_socket_response['url']
 
-    ws = create_connection("{0}".format(websocket_url))
-    print "Receiving..."
-
-sound_options = [ "steve_laugh"]
-
-connect_to_websocket()
+websocket_url = new_websocket_url()
+ws = create_connection("{0}".format(websocket_url))
+print "Receiving"
 
 while True:
     try:
-        result =  json.loads(ws.recv())
+        result = json.loads(ws.recv())
         space = result['payload']['space_id']
+        print result
         if space == 'spc_31879203125199140':
             if result['payload']['direction'] == 1:
                 print "entered office"
                 sound = random.choice(sound_options)
                 print sound
-                subprocess.call(["play", "/home/pi/hackathon-entrance-announcer/sounds/{0}.aif".format(sound)])
-    except KeyboardInterrupt:
-        subprocess.call(["espeak", '"websocket closed, reconnecting" 2>/dev/null'])
-        connect_to_websocket()
+                # subprocess.call(["afplay", "{0}/{1}.aif".format(sound_directory, sound)])   # mac
+                subprocess.call(["play", "{0}/{1}.aif".format(sound_directory, sound)])   # Pi
+
+    except Exception as e:
+        print "{0}".format(e)
+        # subprocess.call(["say", "websocket closed, reconnecting"])   # Pi
+        # subprocess.call(["espeak", '"websocket closed, reconnecting" 2>/dev/null'])   # Pi
+        # get new websocket_url
+        websocket_url = new_websocket_url()
+        ws = create_connection("{0}".format(websocket_url))
+        print "reconnecting"
